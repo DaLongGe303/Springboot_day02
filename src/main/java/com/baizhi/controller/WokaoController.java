d9fa3f2a41765db9004a63067e5ffb536171a976
xlf <260226246@qq.com>
2019-04-12 17:09:22 +0800

    version springbot day2

--- src/main/java/com/baizhi/controller/WokaoController.java
+++ src/main/java/com/baizhi/controller/WokaoController.java
@@ -3,6 +3,7 @@ package com.baizhi.controller;
 import com.baizhi.dao.WokaoDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WokaoController {
